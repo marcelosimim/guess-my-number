60d01e1521c06f7740194a0300c1f70affc86bd2
Marcelo Simim <54868944+marcelosimim@users.noreply.github.com>
2023-07-23 11:13:11 -0300

    different files for android and ios

--- components/ui/Title.android.js
+++ components/ui/Title.android.js
@@ -1,4 +1,4 @@
-import { Text, StyleSheet } from "react-native"
+import { Text, StyleSheet, Platform } from "react-native"
 import Colors from "../../constants/Colors"
 
 export default function Title({children}) {
@@ -11,6 +11,8 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         color: 'white',
         textAlign: 'center',
+        //borderWidth: Platform.OS === 'android' ? 2 : 0,
+        //borderWidth: Platform.select({ios: 0, android: 2}),
         borderWidth: 2,
         borderColor: 'white',
         padding: 12,
